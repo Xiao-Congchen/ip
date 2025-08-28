99cffe32d57c32529fdfb50843c18589247a1db6
LilOnionOrange <xiaocongchen.xcc@gmail.com>
2025-08-28 14:20:02 +0800

    Improve error message for InvalidDateException

--- src/main/java/InvalidDateException.java
+++ src/main/java/InvalidDateException.java
@@ -1,7 +1,7 @@
 public class InvalidDateException extends InvalidException {
     public InvalidDateException(String fieldName) {
         super("That's an invalid " + fieldName + " date/time!\n\t"
-                + "Please follow the format dd/mm/yyyy hhmm!\n\t"
+                + "Please follow the format \"dd/mm/yyyy hhmm\"!\n\t"
                 + "Quack twice and try again~");
     }
 }
