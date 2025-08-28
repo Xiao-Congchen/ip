c006e5ed2ed49268d85dd67bb466b0b9e99f3d5c
LilOnionOrange <xiaocongchen.xcc@gmail.com>
2025-08-28 14:30:11 +0800

    Move friendlyDate function to separate class

---
+++ src/main/java/StringifyDate.java
@@ -0,0 +1,25 @@
+/**
+ * A class to format date outputs
+ *
+ * Helps to convert a LocalDateTime object into a human-friendly date representation
+ */
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StringifyDate {
+    public static String friendlyDate(LocalDateTime dateTime) {
+        String niceDateTime = dateTime.format(DateTimeFormatter.ofPattern("d'th' 'of' MMMM yyyy, h:mma"));
+        if (niceDateTime.startsWith("1th")) {
+            niceDateTime = niceDateTime.replace("th" ,"st");
+        } else if (niceDateTime.startsWith("2th")) {
+            niceDateTime = niceDateTime.replace("th" ,"nd");
+        } else if (niceDateTime.startsWith("3th")) {
+            niceDateTime = niceDateTime.replace("th" ,"rd");
+        }
+        if (niceDateTime.contains(":00")) {
+            niceDateTime = niceDateTime.replace(":00", "");
+        }
+        return niceDateTime;
+    }
+}
