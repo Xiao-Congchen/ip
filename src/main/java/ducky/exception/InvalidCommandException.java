08ba015876d3411b6167aa0a84352ea4f730e843
LilOnionOrange <xiaocongchen.xcc@gmail.com>
2025-09-10 11:47:01 +0800

    Fix wrong ' sign in InvalidCommandException

--- src/main/java/ducky/exception/InvalidCommandException.java
+++ src/main/java/ducky/exception/InvalidCommandException.java
@@ -3,7 +3,7 @@ package ducky.exception;
 public class InvalidCommandException extends DuckyException {
     public InvalidCommandException() {
         super("""
-                Unknown command. My duck brain can’t process that...
+                Unknown command. My duck brain can't process that...
                 \t\
                 You can try:
                 \ttodo, deadline, event, list, mark, unmark, delete, clearall, bye""");
