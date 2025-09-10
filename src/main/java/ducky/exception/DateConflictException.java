dff7d46a8706a7e9751e594e4b0c76123017c53b
LilOnionOrange <xiaocongchen.xcc@gmail.com>
2025-09-10 16:42:46 +0800

    Rename ConflictingDateException to DateConflictException

--- src/main/java/ducky/exception/DateConflictException.java
+++ src/main/java/ducky/exception/DateConflictException.java
@@ -1,7 +1,7 @@
 package ducky.exception;
 
-public class ConflictingDateException extends RuntimeException {
-    public ConflictingDateException(String taskType, String conflictingEvent) {
+public class DateConflictException extends DuckyException {
+    public DateConflictException(String taskType, String conflictingEvent) {
         super("QUACK! The duration of your new " + taskType
                 + "\nClashes with " + conflictingEvent + "!"
                 + "\n Don't over-commit!");
